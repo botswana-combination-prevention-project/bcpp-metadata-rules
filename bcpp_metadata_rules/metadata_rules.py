0f98dab19a88e5c33af41335c3fd0e2d94cc88e7
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-09-07 08:24:45 +0200

    fix venous rule

--- bcpp_metadata_rules/metadata_rules.py
+++ bcpp_metadata_rules/metadata_rules.py
@@ -1,4 +1,5 @@
-from bcpp_labs.labs import microtube_panel, rdb_panel, viral_load_panel, elisa_panel
+from bcpp_labs.labs import microtube_panel, rdb_panel
+from bcpp_labs.labs import venous_panel, viral_load_panel, elisa_panel
 from edc_constants.constants import NO, YES, POS, NEG, FEMALE, IND, NOT_SURE
 from edc_metadata import NOT_REQUIRED, REQUIRED
 from edc_metadata_rules import CrfRuleGroup, CrfRule, RequisitionRuleGroup
@@ -492,17 +493,17 @@ class CrfRuleGroup6(BaseCrfRuleGroup):
         source_model = f'{app_label}.subjectrequisition'
 
 
-# @register()
-# class RequisitionRuleGroup6(BaseRequisitionRuleGroup):
-#
-#     venous_for_vol = RequisitionRule(
-#         predicate=pc.func_requires_venous,
-#         consequence=REQUIRED,
-#         alternative=NOT_REQUIRED,
-#         source_panel=microtube_panel.name,
-#         target_panels=[venous_panel])
-#
-#     class Meta:
-#         app_label = 'bcpp_metadata_rules'
-#         source_model = f'{app_label}.subjectrequisition'
-#         requisition_model = f'{app_label}.subjectrequisition'
+@register()
+class RequisitionRuleGroup6(RequisitionRuleGroup):
+
+    venous_for_vol = RequisitionRule(
+        predicate=pc.func_requires_venous,
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        source_panel=microtube_panel,
+        target_panels=[venous_panel])
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.subjectrequisition'
+        requisition_model = f'{app_label}.subjectrequisition'
